b76fe181143f968d713cc27b88cbc05576e31289
Marc Philipp <mail@marcphilipp.de>
2021-04-05 14:08:15 +0200

    Use Type enum

--- junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/LegacyReportingName.java
+++ junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/LegacyReportingName.java
@@ -5,9 +5,9 @@ import org.opentest4j.reporting.events.api.Context;
 import org.opentest4j.reporting.events.api.QualifiedName;
 import org.opentest4j.reporting.events.core.Metadata;
 
-public class LegacyReportingName extends ChildElement<Metadata, LegacyReportingName> {
+class LegacyReportingName extends ChildElement<Metadata, LegacyReportingName> {
 
-    public static final QualifiedName ELEMENT = QualifiedName.of(JUnitFactory.NAMESPACE, "legacyReportingName");
+    static final QualifiedName ELEMENT = QualifiedName.of(JUnitFactory.NAMESPACE, "legacyReportingName");
 
     LegacyReportingName(Context context, String value) {
         super(context, ELEMENT);
--- junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/Type.java
+++ junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/Type.java
@@ -1,16 +1,17 @@
 package org.junit.platform.reporting.open.xml;
 
+import org.junit.platform.engine.TestDescriptor;
 import org.opentest4j.reporting.events.api.ChildElement;
 import org.opentest4j.reporting.events.api.Context;
 import org.opentest4j.reporting.events.api.QualifiedName;
 import org.opentest4j.reporting.events.core.Metadata;
 
-public class Type extends ChildElement<Metadata, Type> {
+class Type extends ChildElement<Metadata, Type> {
 
     public static final QualifiedName ELEMENT = QualifiedName.of(JUnitFactory.NAMESPACE, "type");
 
-    Type(Context context, String value) {
+    Type(Context context, TestDescriptor.Type type) {
         super(context, ELEMENT);
-        withContent(value);
+        withContent(type.toString());
     }
 }
